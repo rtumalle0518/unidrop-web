d5be1f1cb56ba4e965899c7db26bfe49f3a2fbb1
rtumalle0518 <43913227+rtumalle0518@users.noreply.github.com>
2022-04-04 23:01:07 -0400

    added page routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,  
+  Route,
+  Routes,
+  Link
+} from "react-router-dom";
+import { Home } from './pages/Home';
+import { CreateRoom } from './pages/CreateRoom';
+import { JoinRoom } from './pages/JoinRoom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/create-room' element={<CreateRoom />} />
+        <Route path='/join-room' element={<JoinRoom />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/CreateRoom.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const CreateRoom = () => {
+  return (
+    <div>CreateRoom</div>
+  )
+}
