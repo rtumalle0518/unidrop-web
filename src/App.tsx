b74c38bed00e846fbbd8bac58fea0cc772f470b9
Ayo-100 <mobaisi13@gmail.com>
2022-04-06 11:44:05 -0400

    created navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
-import React from 'react';
-import './App.css';
-import {
-  BrowserRouter as Router,  
-  Route,
-  Routes,
-  Link
-} from "react-router-dom";
-import { Home } from './pages/Home';
-import { CreateRoom } from './pages/CreateRoom';
-import { JoinRoom } from './pages/JoinRoom';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { CreateRoom } from "./pages/CreateRoom";
+import { JoinRoom } from "./pages/JoinRoom";
+import { NavBar } from "./pages/NavBar";
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/create-room' element={<CreateRoom />} />
-        <Route path='/join-room' element={<JoinRoom />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<NavBar />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/create-room" element={<CreateRoom />} />
+				<Route path="/join-room" element={<JoinRoom />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/pages/NavBar.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom"; //client side routing
+
+// function NavBar(){
+//     return(
+
+//     );
+// }
+
+export const NavBar = () => {
+	return (
+		<>
+			<ul>
+				<li>
+					<Link to="/">Home</Link>
+				</li>
+				<li>
+					<Link to="/create-room">Create Room</Link>
+				</li>
+				<li>
+					<Link to="/join-room">Join Room</Link>
+				</li>
+			</ul>
+		</>
+	);
+};
