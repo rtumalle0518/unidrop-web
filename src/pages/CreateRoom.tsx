36aaee18d5727b8fd7ed0f3961a858e3c87aca19
rtumalle0518 <43913227+rtumalle0518@users.noreply.github.com>
2022-04-05 23:11:45 -0400

    added material ui and socket io

--- src/pages/CreateRoom.tsx
+++ src/pages/CreateRoom.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 export const CreateRoom = () => {
   return (
-    <div>CreateRoom</div>
+    <>
+      <div>CreateRoom</div>
+      <Button>Create Room</Button>
+    </>
   )
 }
