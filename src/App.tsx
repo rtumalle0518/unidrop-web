2e161efd57f005f8f1ce2bd9df888cf205ed8cff
rtumalle0518 <43913227+rtumalle0518@users.noreply.github.com>
2022-04-17 13:52:09 -0400

    Converted JoinRoomTest to JoinRoom

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,8 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { CreateRoom } from "./pages/CreateRoom";
-import { JoinRoom } from "./pages/JoinRoom";
 import { NavBar } from "./components/NavBar";
-import { JoinRoomTest } from "./pages/JoinRoomTest";
+import { JoinRoom } from "./pages/JoinRoom";
 import { createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 
 const theme = createTheme({
@@ -26,7 +25,6 @@ function App() {
 						<Route path="/" element={<Home />} />
 						<Route path="/create-room" element={<CreateRoom />} />
 						<Route path="/join-room" element={<JoinRoom />} />
-						<Route path="/join-room-test" element={<JoinRoomTest />} />
 					</Routes>
 				</ThemeProvider>
 			</NavBar>
--- src/pages/JoinRoom.tsx
+++ src/pages/JoinRoom.tsx
@@ -1,33 +1,59 @@
-import React from "react";
+import { useEffect, MouseEvent, useState } from 'react'
+import { io } from 'socket.io-client'
+import { Button, TextField, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { FileShare } from './FileShare';
+
+const StyledButton = styled(Button)`
+    height: 100%;
+    width: 100%;
+`;
+const socket = io('http://localhost:4000');
 
 export const JoinRoom = () => {
-	return (
-		<body>
-			<div className="app">
-				<div className="screen join-screen active">
-					<div className="form">
-						<h2>Share your files</h2>
-						<div className="form-input">
-							<label>Join ID</label>
-							<input type="text" id="join-id" />
-						</div>
-						<div className="form-input">
-							<button>Connect</button>
-						</div>
-					</div>
-				</div>
-				<div className="screen fs-screen">
-					<div className="files-list">
-						<div className="title">Shared Files</div>
-					</div>
-				</div>
-			</div>
-			<script type="text/javascript" src="socket.io/socket.io.js"></script>
-			<script
-				type="text/javascript"
-				src="https://cdnjs.cloudflare.com/ajax/libs/downloadjs/1.4.1/download.min.js"
-			></script>
-			<script type="text/javascript" src="reciever.js"></script>
-		</body>
-	);
-};
+    const [value, setValue] = useState('');
+    const [roomReady, setRoomReady] = useState(false);
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log('Hello from Join Room')
+        })
+        socket.on('in-room', () => {
+            setRoomReady(true);
+            console.log('In room')
+        })
+        socket.on("connect_error", (err) => {
+            console.log(`connect_error due to ${err.message}`);
+        })
+        socket.on('both-in-room', () => {
+            console.log("We should get same message");
+        })
+    }, []);
+    
+    const handleClick = (event: MouseEvent): void => {
+        console.log(value)
+        socket.emit('join-room', {
+            roomId: value,
+            socketId: socket.id
+        })
+        
+    }
+    return (
+        <>
+            {roomReady ? <FileShare socket={socket} roomId={value}/> :
+                <Grid container spacing={0}>
+                    <Grid item xs={4}>
+                        <TextField 
+                            label="Enter Room ID" 
+                            variant="outlined"
+                            value={value}
+                            onChange={e => setValue(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item xs={2}>
+                        <StyledButton onClick={handleClick}>Join Room</StyledButton>
+                    </Grid>
+                </Grid> 
+            }
+        </>
+    )
+}
